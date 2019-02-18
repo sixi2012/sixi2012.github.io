ed6fd35f118d9ee16ecc8df9a7aa23741e4a632b
sixi2010 <luo2010yd@163.com>
2019-02-18 17:35:48 +0800

    Site updated: 2019-02-18 17:35:48

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/sixi2012/Blog-Photos/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/sixi2012/Blog-Photos/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/sixi2012/Blog-Photos/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/sixi2012/Blog-Photos/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           //src += '.jpg';
